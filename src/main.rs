d756529c37126d350d313b4e220e2e9bb6b1c74e
Michael Rommel <rommel@layer-7.net>
2026-03-06 17:14:01 +0100

    feat: limiter cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 use anyhow::{Context, Result};
-use rand::Rng;
-use std::{env, time::Instant};
+use async_stream::stream;
+use governor::{Quota, RateLimiter};
+use rand::RngExt;
+use socket2::{Domain, Protocol, Socket, Type};
+use std::{
+    env,
+    net::{SocketAddr, TcpStream as StdTcpStream},
+    // num::NonZeroU32,
+    pin::Pin,
+    sync::Arc,
+    time::Instant,
+};
 use suppaftp::{tokio::AsyncFtpStream, types::Mode};
 use tokio::{
     fs::File,
     io::{AsyncWriteExt, BufWriter},
+    net::TcpStream,
     task::JoinSet,
     time::{Duration, sleep},
 };
 use tokio_stream::StreamExt;
 use tokio_util::io::{ReaderStream, StreamReader};
+
+#[derive(Debug, Copy, Clone)]
+struct RateLimiterConfig {
+    limiter: bool,
+    size: u32,
+    interval: u64,
+    mss: u32,
+}
+
+impl RateLimiterConfig {
+    fn new(limiter: bool, size: u32, interval: u64, mss: u32) -> Self {
+        Self {
+            limiter,
+            size,
+            interval,
+            mss,
+        }
+    }
+}
 
 async fn setup_files() -> Result<u32> {
     let filesize = env::var("AEROSTRESS_SIZE").unwrap_or("10".to_string());
@@ -55,8 +85,7 @@ async fn write_async(
     num: i32,
     filename: String,
     destination: String,
-    brake: u64,
-    chunk: u64,
+    rlc: RateLimiterConfig,
 ) -> Result<u64> {
     let mut ftp_stream = AsyncFtpStream::connect(destination)
         .await
@@ -72,22 +101,90 @@ async fn write_async(
     let mut file = File::open("mediumfile.dat")
         .await
         .with_context(|| format!("Source file {}-{} could not be opened", batch, num))?;
-    println!("Stream {}-{} read source file", batch, num);
+    println!("Stream {}-{} opened source file", batch, num);
 
     let bytes_written: u64;
-    if brake > 0 {
-        let reader_stream = ReaderStream::with_capacity(file, (chunk * 1024) as usize);
-        let throttled_reader = reader_stream.throttle(Duration::from_millis(brake));
-        let async_reader = StreamReader::new(throttled_reader);
-        tokio::pin!(async_reader);
+    if rlc.limiter {
+        ftp_stream = ftp_stream.passive_stream_builder(move |addr: SocketAddr| {
+            let fut = async move {
+                let socket =
+                    Socket::new(Domain::for_address(addr), Type::STREAM, Some(Protocol::TCP))
+                        .map_err(suppaftp::FtpError::ConnectionError)?;
+                if rlc.mss > 0 {
+                    socket
+                        .set_tcp_mss(rlc.mss)
+                        .map_err(suppaftp::FtpError::ConnectionError)?;
+                }
+                socket
+                    .connect(&addr.into())
+                    .map_err(suppaftp::FtpError::ConnectionError)?;
+                let std_stream: StdTcpStream = socket.into();
+                std_stream
+                    .set_nonblocking(true)
+                    .map_err(suppaftp::FtpError::ConnectionError)?;
+                TcpStream::from_std(std_stream).map_err(suppaftp::FtpError::ConnectionError)
+            };
+            Box::pin(fut)
+                as Pin<
+                    Box<
+                        dyn futures::Future<
+                                Output = Result<tokio::net::TcpStream, suppaftp::FtpError>,
+                            > + Send
+                            + Sync,
+                    >,
+                >
+        });
         let mut data_stream = ftp_stream.put_with_stream(filename.clone()).await?;
-        bytes_written = tokio::io::copy(&mut async_reader, &mut data_stream)
-            .await
-            .with_context(|| format!("File {}-{} could not be streamed", batch, num))?;
-        ftp_stream
-            .finalize_put_stream(data_stream)
-            .await
-            .with_context(|| format!("File {}-{} could not be finalized", batch, num))?;
+
+        let mut reader_stream;
+        if rlc.size > 0 {
+            reader_stream = ReaderStream::with_capacity(file, (rlc.size) as usize);
+        } else {
+            reader_stream = ReaderStream::new(file);
+        }
+
+        if rlc.interval > 0 {
+            // if rlc.interval > 0 {
+            //     let throttled_reader = reader_stream.throttle(Duration::from_millis(rlc.interval));
+            // }
+            // let async_reader = StreamReader::new(throttled_reader);
+            let quota = Quota::with_period(Duration::from_millis(rlc.interval)).unwrap();
+            let limiter = Arc::new(RateLimiter::direct(quota));
+            let throttled_reader = stream! {
+                while let Some(chunk) = reader_stream.next().await {
+                    limiter.until_ready().await;
+                    yield chunk;
+                }
+            };
+            let async_reader = StreamReader::new(throttled_reader);
+            tokio::pin!(async_reader);
+            println!(
+                "Stream {}-{} created rate limited stream: interval {}, chunk size {}, mss {}",
+                batch, num, rlc.interval, rlc.size, rlc.mss
+            );
+
+            bytes_written = tokio::io::copy(&mut async_reader, &mut data_stream)
+                .await
+                .with_context(|| format!("File {}-{} could not be streamed", batch, num))?;
+            ftp_stream
+                .finalize_put_stream(data_stream)
+                .await
+                .with_context(|| format!("File {}-{} could not be finalized", batch, num))?;
+        } else {
+            let async_reader = StreamReader::new(reader_stream);
+            tokio::pin!(async_reader);
+            println!(
+                "Stream {}-{} created stream: interval {}, chunk size {}, mss {}",
+                batch, num, rlc.interval, rlc.size, rlc.mss
+            );
+            bytes_written = tokio::io::copy(&mut async_reader, &mut data_stream)
+                .await
+                .with_context(|| format!("File {}-{} could not be streamed", batch, num))?;
+            ftp_stream
+                .finalize_put_stream(data_stream)
+                .await
+                .with_context(|| format!("File {}-{} could not be finalized", batch, num))?;
+        }
     } else {
         bytes_written = ftp_stream
             .put_file(filename.clone(), &mut file)
@@ -98,20 +195,6 @@ async fn write_async(
     ftp_stream.quit().await?;
     Ok(bytes_written)
 }
-
-// fn write_sync() {
-//     // Create a connection to an FTP server and authenticate to it.
-//     let mut ftp_stream = FtpStream::connect("127.0.0.1:2121").unwrap();
-//     ftp_stream.login("rdiagftp", "siemens").unwrap();
-
-//     // Store (PUT) a file from the client to the current working directory of the server.
-//     let mut reader = Cursor::new("Hello from the Rust \"ftp\" crate!".as_bytes());
-//     let _ = ftp_stream.put_file("greeting.txt", &mut reader);
-//     println!("Successfully wrote greeting.txt");
-
-//     // Terminate the connection to the server.
-//     let _ = ftp_stream.quit();
-// }
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -134,15 +217,24 @@ async fn main() -> Result<()> {
     let d: u64 = delay
         .parse()
         .expect("AEROSTRESS_DELAY parameter is not a number");
-    let brake = env::var("AEROSTRESS_THROTTLE").unwrap_or("0".to_string());
-    let t: u64 = brake
+    let limiter = env::var("AEROSTRESS_LIMITER").unwrap_or("false".to_string());
+    let l: bool = limiter
         .parse()
-        .expect("AEROSTRESS_THROTTLE parameter is not a number");
+        .expect("AEROSTRESS_LIMITER parameter is not a boolean");
     let chunk = env::var("AEROSTRESS_CHUNK").unwrap_or("4".to_string());
-    let c: u64 = chunk
+    let c: u32 = chunk
         .parse()
         .expect("AEROSTRESS_CHUNK parameter is not a number");
+    let interval = env::var("AEROSTRESS_INTERVAL").unwrap_or("0".to_string());
+    let i: u64 = interval
+        .parse()
+        .expect("AEROSTRESS_INTERVAL parameter is not a number");
+    let mss = env::var("AEROSTRESS_MSS").unwrap_or("1460".to_string());
+    let m: u32 = mss
+        .parse()
+        .expect("AEROSTRESS_MSS parameter is not a number");
 
+    let rlc = RateLimiterConfig::new(l, c, i, m);
     let mut error_count: u64 = 0;
 
     // ramp up the load in steps of batches
@@ -158,11 +250,14 @@ async fn main() -> Result<()> {
                 // wait inside the task before starting the ftp transfer
                 sleep(Duration::from_secs(delay)).await;
                 let f = format!("testfile_{:02}_{:04}.txt", j, i);
-                let bytes_written = write_async(j, i, f, destination, t, c).await?;
+                let start_time = Instant::now();
+                let bytes_written = write_async(j, i, f, destination, rlc).await?;
+                let elapsed = start_time.elapsed();
                 println!(
-                    "Task {} finished, {:.3} MBytes",
+                    "Task {} finished, {:.3} MiBytes, {:.3} kibit/s",
                     i,
-                    bytes_written / 1024 / 1024
+                    bytes_written / 1024 / 1024,
+                    (bytes_written * 8) as u128 / elapsed.as_millis(),
                 );
                 Ok(bytes_written)
             });
@@ -192,7 +287,7 @@ async fn main() -> Result<()> {
     }
 
     println!(
-        "All tasks joined. Total elapsed time: {:?}, total GB: {:?}, errors: {}",
+        "All tasks joined. Total elapsed time: {:?}, total GiB: {:?}, errors: {}",
         start_time.elapsed(),
         sum_bytes / 1024 / 1024 / 1024,
         error_count
